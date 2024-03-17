6ea34d4247bb222ae9c3f0411e7e4a21a9358423
Sahil <sahilanand568@gmail.com>
2024-03-17 11:50:28 -0700

    latest changes

--- src/pages/Competition/Competition.jsx
+++ src/pages/Competition/Competition.jsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import "./competition.scss"
+import Atop from "../../Img/arrowTop.png";
+import Aright from "../../Img/arrowRight.png";
+import Abottom from "../../Img/arrowBottom.png";
+import Aleft from "../../Img/arrowLeft.png";
+import RCLogo from "../../Img/RCLogo.png";
+import Atxt1 from "../../Img/Ctxt1.png"
+import Atxt2 from "../../Img/Ctxt2.png"
+import Atxt3 from "../../Img/Ctxt3.png"
+import Atxt4 from "../../Img/Ctxt4.png"
+import Atxt5 from "../../Img/Ctxt5.png"
 
-const Competition = () => {
+const Competition = ({navID}) => {
+  const[flag,setFlag]=useState(0);
+
+  
+  useEffect(()=>{
+    let x=setTimeout(()=>{
+      setFlag(1);
+    },500);
+
+    let y=setTimeout(()=>{
+    setFlag(2);
+    },1000);
+
+    if(navID !== 8)
+    setFlag(0);
+
+    return ()=>{
+      clearTimeout(x);
+      clearTimeout(y);
+    }
+  })
+
   return (
-    <div>Competition</div>
+    <div className='competition-main-comp'>
+       <img className='rc-logo' src={RCLogo} alt="" style={{transform:flag >=1 && "rotate(0deg) scale(1)"}} />
+       <img className='a-top' src={Atop} alt="" style={{transform:flag >=1 && "translateY(-80%) scale(1"}} />
+       <img className='a-right' src={Aright} alt="" style={{transform:flag >=1 && "translateX(50%) scale(1"}} />
+       <img className='a-bottom' src={Abottom} alt="" style={{transform:flag >=1 && "translateY(80%) scale(1"}} />
+       <img className='a-left' src={Aleft} alt="" style={{transform:flag >=1 && "translateX(-50%) scale(1"}} />
+       <div className='comp-outer-circle' 
+       style={{transform: flag >=2 && "translate(-150%,-130%) rotate(0) scale(1)"}}
+       >
+        <div className='comp-inner-circle'>
+          <div className='comp-inner-most'>
+            <p>Property</p>
+            <p>AGGREGATION</p>
+          </div>
+        </div>
+       </div>
+       <div className='comp-outer-circle' style={{transform:flag >=2 &&  "translate(50%,-130%) rotate(0) scale(1)",justifyContent:"flex-start",background: "linear-gradient(235deg, rgba(255,255,255,1) 0%, rgba(0,0,0,1) 55%)"}}>
+        <div className='comp-inner-circle'>
+          <div className='comp-inner-most'>
+            <p>Legal</p>
+            <p>SERVICES</p>
+          </div>
+        </div>
+       </div>
+       <div className='comp-outer-circle' style={{transform:flag >=2 &&  "translate(50%,30%) rotate(0) scale(1)",justifyContent:"flex-start",background: "linear-gradient(325deg, rgba(255,255,255,1) 0%, rgba(0,0,0,1) 55%)"}}>
+        <div className='comp-inner-circle'>
+          <div className='comp-inner-most'>
+            <p>Broker</p>
+            <p>MANAGEMENT</p>
+          </div>
+        </div>
+       </div>
+       <div className='comp-outer-circle' style={{transform:flag >=2 &&  "translate(-150%,30%) rotate(0) scale(1)",background: "linear-gradient(20deg, rgba(255,255,255,1) 0%, rgba(0,0,0,1) 55%)"}}>
+        <div className='comp-inner-circle'>
+          <div className='comp-inner-most'>
+            <p>Credit</p>
+            <p>BUREAU</p>
+          </div>
+        </div>
+       </div>
+
+    </div>
   )
 }
 
--- src/pages/Scope/Scope.jsx
+++ src/pages/Scope/Scope.jsx
@@ -40,17 +40,53 @@ const Scope = ({ navID }) => {
     arr1.push(i);
   }
 
-  console.log(count, "sdfhjkbsdijh");
+  // console.log(count, "sdfhjkbsdijh");
 
   return (
     <div className="scope-main-container">
       <div className="left-scope">
         <img className="lappy-scope" src={LappyScope} alt="" />
-
+        <div className="scope-heading">
+        <h2 > MARKET</h2>
+        <h2 > ANALYSIS</h2>
+        </div>
         <div className="left-Sbottom">
-         <img className="first-eye" src={FirstE} alt=""/>
-         <img className="second-eye" src={SecondE} alt="" />
-         <img className="third-eye" src={ThirdE} alt="" />
+         <img className="first-eye" src={FirstE} alt="" />
+         <img className="second-eye" src={SecondE} alt="" style={{transform:count >= 1 && "translateX(27vw)"}} />
+         <img className="third-eye" src={ThirdE} alt=""  style={{transform:count >= 1 && "translateX(54vw)"}}/>
+         <div className="Stxt-container" style={{opacity:count >=1 && 1}}>
+         <div className="Stxt-head">
+            <h2>TAM</h2>
+            <h2>Total Available <br/> Market</h2>
+            <div className="Scope-lg-border"></div>
+          </div>
+          <div className="Sbtn-filler" style={{transform:count >=1 && "scale(1)"}}>
+            <div className="Sbtn-part-filler"></div>
+          </div>
+          <h1>4.6 <span>CR</span></h1>
+         </div>
+         <div className="Stxt-container" style={{transform:"translateX(33vw)",opacity:count >=1 && 1}}>
+         <div className="Stxt-head">
+            <h2>TAM</h2>
+            <h2>Total Available <br/> Market</h2>
+            <div className="Scope-lg-border"></div>
+          </div>
+          <div className="Sbtn-filler" style={{transform:count >=1 && "scale(1)"}}>
+            <div className="Sbtn-part-filler"></div>
+          </div>
+          <h1>4.6 <span>CR</span></h1>
+         </div>
+         <div className="Stxt-container" style={{transform:"translateX(65vw)",opacity:count >=1 && 1}}>
+         <div className="Stxt-head">
+            <h2>TAM</h2>
+            <h2>Total Available <br/> Market</h2>
+            <div className="Scope-lg-border"></div>
+          </div>
+          <div className="Sbtn-filler" style={{transform:count >=1 && "scale(1)"}}>
+            <div className="Sbtn-part-filler"></div>
+          </div>
+          <h1>4.6 <span>CR</span></h1>
+         </div>
          </div>
       </div>
       <div className="right-scope">
@@ -169,7 +205,7 @@ const Scope = ({ navID }) => {
         <p>Lorem ipsum <br/> dolor</p>
         <p>Lorem ipsum <br/> dolor</p>
       </div>
-      <div className="tam-txt">
+      <div className="tam-txt" style={{transform: count >= 1 && "scale(0)"}}>
         <h1>TAM, SAM, SOM</h1>
         <h1 className="tam-analysis">ANALYSIS</h1>
       </div>
